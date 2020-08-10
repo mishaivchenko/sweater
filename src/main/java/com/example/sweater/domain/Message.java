5a06e2dd2f9177ddb6c629e99fe99737d56cd84d
mishaivchenko <mishaivchenko@gmail.com>
2020-08-10 12:33:47 +0300

    1) add security 2) add relations 3) ...

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,27 +1,33 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    private String text;
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
     }
 
-    private String text;
 
-private String tag;
+    public String getAuthorName(){
+        return author != null? author.getUsername(): "<none>";
+    }
 
     public Integer getId() {
         return id;
